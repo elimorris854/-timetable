e05406e28c85065dc8c940db5e491b6e667ef9ee
EliMorris854 <133006577+EliMorris854@users.noreply.github.com>
2025-10-31 13:19:38 +0000

    Test Commit 3

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@
 void main() {
   //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
   // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("I've changed the counter back to 10 once more"));
+  IO.println(String.format("I've changed the counter back to 2"));
 
-  for (int i = 1; i <= 10; i++) {
+  for (int i = 1; i <= 2; i++) {
     //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
     // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
     IO.println("i = " + i);
