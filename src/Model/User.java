650d34238c4ccc72c53fabf24ee13d00ce970650
jackbuttery <24426962@studentmail.ul.ie>
2025-11-13 10:18:13 +0000

    Added methods and javadocs for user and its child classes

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,4 +1,56 @@
 package Model;
+/**
+ * Abstract base class representing a system user.
+ * Provides common attributes and methods shared by all user types.
+ */
+public abstract class User {
 
-public class User {
+    /** Unique identifier for the user. */
+    protected String id;
+
+    /** Full name of the user. */
+    protected String name;
+
+    /** Email address of the user. */
+    protected String email;
+
+    /**
+     * Constructor for creating a user.
+     *
+     * @param id unique identifier
+     * @param name full name
+     * @param email email address
+     */
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // ----- Getters -----
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // ----- Abstract -----
+
+    /**
+     * Returns a string describing the user type.
+     * @return user role, e.g. "Student", "Lecturer", "Admin".
+     */
+    public abstract String getRole();
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s (%s)", getRole(), name, email);
+    }
 }
