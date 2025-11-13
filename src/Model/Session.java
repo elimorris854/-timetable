537080678eba2d01d69ac2ca473ac5eb693766d8
EliMorris854 <133006577+EliMorris854@users.noreply.github.com>
2025-11-13 17:21:35 +0000

    Session and Timetable classes

--- src/Model/Session.java
+++ src/Model/Session.java
@@ -1,4 +1,85 @@
 package Model;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.List;
+
 public class Session {
+    private String sessionID;
+    private String moduleCode;
+    private String sessionType;
+    private String lecturerID;
+    private String roomID;
+    private List<String> studentGroupIDs;
+    private DayOfWeek day;
+    private LocalTime startTime;
+    private int sessionDuration;
+
+    public Session(String sessionID,String moduleCode,String sessionType,String lecturerID,String roomID, List<String> studentGroupIDs, DayOfWeek day, LocalTime startTime,int sessionDuration){
+        this.sessionID=sessionID;
+        this.moduleCode = moduleCode;
+        this.sessionType = sessionType;
+        this.lecturerID = lecturerID;
+        this.roomID=roomID;
+        this.studentGroupIDs=studentGroupIDs;
+        this.day = day;
+        this.startTime = startTime;
+        this.sessionDuration=sessionDuration;
+    }
+
+    public String getSessionID(){
+        return sessionID;
+    }
+
+    public String getModuleCode(){
+        return  moduleCode;
+    }
+
+    public String getSessionType(){
+        return sessionType;
+    }
+
+    public String getLecturerID(){
+        return  lecturerID;
+    }
+
+    public String getRoomID(){
+        return roomID;
+    }
+
+    public List<String> getStudentGroupIDs(){
+        return studentGroupIDs;
+    }
+
+    public DayOfWeek getDay(){
+        return day;
+    }
+
+    public LocalTime getStartTime(){
+        return startTime;
+    }
+
+    public int getSessionDuration(){
+        return sessionDuration;
+    }
+
+    public boolean overLapsWith(Session other){
+        if(!this.day.equals(other.day)){
+            return false;
+        }
+
+        LocalTime thisEndTime= this.startTime.plusMinutes(this.sessionDuration);
+        LocalTime otherEndTime=other.startTime.plusMinutes(other.sessionDuration);
+
+        if (thisEndTime.isBefore(other.startTime) || thisEndTime.equals(this.startTime)){
+            return false;
+        }
+
+        if(otherEndTime.isBefore((this.startTime)) || otherEndTime.equals(this.startTime)){
+            return false;
+        }
+        return true;
+    }
+
+
 }
--- src/Model/Timetable.java
+++ src/Model/Timetable.java
@@ -7,4 +7,39 @@ public class Timetable{
     private String ownerID;
     private List<Session> schedulesSessions;
 
+    public Timetable(String ownerID){
+        this.ownerID = ownerID;
+        this.schedulesSessions = new ArrayList<>();
+    }
+
+    public String getOwnerID(){
+        return ownerID;
+    }
+
+    public List<Session> getSchedulesSessions(){
+        return new ArrayList<>(schedulesSessions);
+    }
+
+    public boolean addSession(Session newSession){
+        if(checkConflict(newSession)){
+            return false;
+        }
+        schedulesSessions.add(newSession);
+        return true;
+    }
+
+    public void removeSession(String sessionId){
+    }
+
+    public boolean checkConflict(Session newSession){
+        for(int i=0;i<schedulesSessions.size();i++){
+            Session existingSession=schedulesSessions.get(i);
+            if(newSession.overLapsWith(existingSession)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
