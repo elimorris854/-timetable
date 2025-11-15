57c2933ef54002f0ca1ba98e31dd9484734581f7
Evan Hickey <150790020+Ridicilo@users.noreply.github.com>
2025-11-15 19:04:39 +0000

    Update Programme.java

--- src/Model/Programme.java
+++ src/Model/Programme.java
@@ -1,5 +1,12 @@
 package Model;
 
+import java.util.List;
+
+/**
+ * Represents a programme with a number of module assigned to it.
+ * Each programme has one student group.
+ */
+
 public class Programme{
   
 }
