4670659807b55a13d5e658dc4dd46f05121e9a82
Evan Hickey <150790020+Ridicilo@users.noreply.github.com>
2025-11-15 19:04:46 +0000

    Update StudentGroup.java

--- src/Model/StudentGroup.java
+++ src/Model/StudentGroup.java
@@ -1,5 +1,12 @@
 package Model;
 
+import java.util.List;
+
+/**
+ * Represents a group of students who share a programme.
+ * A student group has students and has an associated timetable.
+ */
+
 public class StudentGroup{
 
 }
