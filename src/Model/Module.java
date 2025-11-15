b0d5ee747d90df9ad2535dd50b791fd48dc0223a
Evan Hickey <150790020+Ridicilo@users.noreply.github.com>
2025-11-15 19:04:43 +0000

    Update Module.java

--- src/Model/Module.java
+++ src/Model/Module.java
@@ -1,5 +1,12 @@
 package Model;
 
+import java.util.List;
+
+/**
+ * Represents a module with a specific number of lectures/labs.
+ * A module can be used in multiple programmes.
+ */
+
 public class Module {
 
 }
